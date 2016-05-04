bac15608689489bdc376a217495b3df9045820bc
Akram8931 <akramabdou@Akrams-MacBook-Air.local>
2016-05-04 05:35:26 -0400

    Myjiff aka Aurora

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+// all constants start with let 
+// all cariables start with var (value can vary)
+let numberOfEggsInDozen : Int = 12
+var breakfastSpecial : String = "sara and akram"
+
+"h"
+println("Hello world!")
+"h"
+
+println("Hello World")
+println("I have \(numberOfEggsInDozen) eggs since i just bought a fresh yesterday")
+println("The \(breakfastSpecial) is the special menu item for today")
